2d85c4321a58afaff3f4e7e97db1fcaacf580a1e
niranjana <niranjana@qburst.com>
2022-02-14 15:57:16 +0530

    removed workers

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 
-celery -A core worker -l info
+# celery -A core worker -l info
 
-celery -A core worker -l info -B
+# celery -A core worker -l info -B
 
 gunicorn core.wsgi --bind 0.0.0.0:$PORT
 
